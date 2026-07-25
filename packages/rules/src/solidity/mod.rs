a3aecdbe829846f7a29e75ba504041a43f679f8a
Ogunmodede Joel Taiwo <jotelfootball@gmail.com>
2026-07-25 21:04:51 +0100

    feat(analysis): build multi-contract execution call-graph generator (Issue #604)

--- packages/rules/src/solidity/mod.rs
+++ packages/rules/src/solidity/mod.rs
@@ -2,7 +2,6 @@ pub mod dynamic_array_deletion;
 pub mod mapping_iteration;
 pub mod state_variable_packing;
 pub mod uint8_vs_uint256;
-pub mod mapping_iteration;
 pub mod abi_encode;
 
 pub use dynamic_array_deletion::DynamicArrayDeletionRule;
